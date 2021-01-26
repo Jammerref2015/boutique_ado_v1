b187da67a5c90834e8bcc71afb1283e7c2bf4d07
Adrian Looney <adrian.looney@gmail.com>
2021-01-26 18:41:23 +0000

    Added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -184,6 +184,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+    # Cache settings
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000', 
+    }
+
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'ades-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-west-1'
@@ -191,7 +197,6 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-        
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
